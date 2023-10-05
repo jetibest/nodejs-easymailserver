6c573ce400492ef53b9a23c5426870ee88d70225
Master Yeti <jetibest@github.com>
2023-10-05 16:39:23 +0200

    Fix TLS certs renewal hook.

--- letsencrypt/001-easymailserver.sh
+++ letsencrypt/001-easymailserver.sh
@@ -4,14 +4,19 @@ set -e # exit on any failure
 
 wd="/srv/easymailserver/"
 domain_name="$(head -n1 "$wd"config/me || hostname -d)"
-pem_file="$wd"config/tls/"$domain_name".pem
+pem_filename="$domain_name".pem
+pem_file="$wd"config/tls/"$pem_filename"
 
 echo "Deploying renewed certificates from $domain_name to easymailserver..."
 
+# note: currently the config/tls directory may only contain PEM files, and nothing else (no subdirectories, or the reading of certificates will fail in Haraka/tls_socket.js::exports.get_certs_dir)
+
 # copy certificate of the domain to the config/tls directory
 [ -e "$wd"config/tls ] || mkdir -p "$wd"config/tls
+[ -e "$wd"config/tls-old ] || mkdir -p "$wd"config/tls-old
 cat /etc/letsencrypt/live/"$domain_name"/privkey.pem /etc/letsencrypt/live/"$domain_name"/fullchain.pem >"$pem_file".new
 chown -R vmail:vmail "$wd"config/tls
+chown -R vmail:vmail "$wd"config/tls-old
 
 # check if certificate for this domain was actually renewed:
 if cmp --silent -- "$pem_file".new "$pem_file"
@@ -25,7 +30,7 @@ then
 fi
 
 # make a backup of the current pem-file
-cp -f "$pem_file" "$pem_file".old
+cp -f "$pem_file" "$wd"config/tls-old/"$pem_filename"
 
 # replace current pem-file with new pem-file
 mv -f "$pem_file".new "$pem_file"
